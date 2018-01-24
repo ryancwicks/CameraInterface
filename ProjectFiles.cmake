bfa9691647e0fb3647a1c3cf5896fafafe5d3fd3
Ryan Wicks <ryancwicks@gmail.com>
2018-01-23 19:12:44 -0500

    Added initial file set.

---
+++ ProjectFiles.cmake
@@ -0,0 +1,23 @@
+# Copyright (c) 2018 Ryan Wicks (ryancwicks@gmail.com)
+# Distributed under the MIT License.
+# See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+
+# Set project source files.
+set(SRC
+  "${SRC_PATH}/CameraBase.cpp"
+)
+
+set (HEADER
+    "${INCLUDE_PATH}/CameraInterface/CameraBase.hpp"
+)
+
+# Set project main file.
+set(MAIN_SRC
+  #"${SRC_PATH}/main.cpp"
+)
+
+# Set project test source files.
+set(TEST_SRC
+  "${TEST_SRC_PATH}/testCameraBase.cpp"
+)
+
